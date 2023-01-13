153128965f698cd3a0aa70836beb98b4ece040f8
Heulween19 <anayang1907@gmail.com>
2023-01-13 16:51:01 +0700

    add Threadpool project

---
+++ Practice/Task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Threadpool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Queue the task.
+            ThreadPool.QueueUserWorkItem(ThreadProc);
+            Console.WriteLine("Main thread does some work, then sleeps.");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Main thread exits.");
+        }
+
+       static void ThreadProc(object state)
+        {
+            // No state object was passed to QueueUserWorkItem, so stateInfo is null.
+            Console.WriteLine("Hello from the thread pool.");
+        }
+    }
+}
