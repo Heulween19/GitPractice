83943cb8c4d8b698ac29e1bb6f0dc01b0dae504e
Heulween19 <anayang1907@gmail.com>
2023-01-16 09:45:34 +0700

    compare threadpool vs threade

--- Practice/Task/Program.cs
+++ Practice/Task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace Threadpool
@@ -8,17 +9,52 @@ namespace Threadpool
         static void Main(string[] args)
         {
             // Queue the task.
-            ThreadPool.QueueUserWorkItem(ThreadProc);
-            Console.WriteLine("Main thread does some work, then sleeps.");
-            Thread.Sleep(1000);
+            //ThreadPool.QueueUserWorkItem(ThreadProc);
+            //Console.WriteLine("Main thread does some work, then sleeps.");
+            //Thread.Sleep(1000);
 
-            Console.WriteLine("Main thread exits.");
+            //Console.WriteLine("Main thread exits.");
+
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("Execution using Thread");
+            stopwatch.Start();
+            MethodWithThread();
+            stopwatch.Stop();
+            Console.WriteLine("Time consumed by MethodWithThread is : " +
+                                 stopwatch.ElapsedTicks.ToString());
+
+            stopwatch.Reset();
+            Console.WriteLine("Execution using Thread Pool");
+            stopwatch.Start();
+            MethodWithThreadPool();
+            stopwatch.Stop();
+            Console.WriteLine("Time consumed by MethodWithThreadPool is : " +
+                                 stopwatch.ElapsedTicks.ToString());
+
+            Console.Read();
+
         }
 
        static void ThreadProc(object state)
         {
             // No state object was passed to QueueUserWorkItem, so stateInfo is null.
             Console.WriteLine("Hello from the thread pool.");
+        }
+        static void MethodWithThreadPool()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc));
+            }
+        }
+
+        static void MethodWithThread()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(ThreadProc);
+                thread.Start();
+            }
         }
     }
 }
